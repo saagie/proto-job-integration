d3bd1f313a8faa4f657857ec066aac5aa4c19fdc
Franck CARON <franck.caron@saagie.com>
2018-07-18 11:59:45 +0200

    (fix) #12 : Making password optional on BasicSecurer

--- src/main/kotlin/io/saagie/poc/infra/right/common/securer/BasicSecurer.kt
+++ src/main/kotlin/io/saagie/poc/infra/right/common/securer/BasicSecurer.kt
@@ -3,7 +3,7 @@ package io.saagie.poc.infra.right.common.securer
 import io.saagie.poc.infra.right.common.encode64
 
 
-class BasicSecurer(private val username: String, private val password: String): AbstractSecurer() {
+class BasicSecurer(private val username: String, private val password: String = ""): AbstractSecurer() {
     // METHOD
     override fun getAuthorization(): String = "Basic ${encode64("$username:$password")}"
 }
